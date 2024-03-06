845c630e8898b7762c50df39d9bac4c4a3eaaba9
Adam Pagels <pagels@hotmail.ca>
2024-03-06 18:38:12 +0000

    First commit

---
+++ marvel-everything/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  marvel-everything
+//
+//  Created by Adam Pagels on 2024-03-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ marvel-everything/marvel_everythingApp.swift
@@ -0,0 +1,17 @@
+//
+//  marvel_everythingApp.swift
+//  marvel-everything
+//
+//  Created by Adam Pagels on 2024-03-06.
+//
+
+import SwiftUI
+
+@main
+struct marvel_everythingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
