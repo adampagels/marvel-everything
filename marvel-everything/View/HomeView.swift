01eae2955fb2c5e3b68403ac2b1fb964c4b83cbe
Adam Pagels <pagels@hotmail.ca>
2024-03-15 11:44:47 +0000

    Display Cards for characters in Grid

--- marvel-everything/View/HomeView.swift
+++ marvel-everything/View/HomeView.swift
@@ -9,13 +9,55 @@ import SwiftUI
 
 struct HomeView: View {
     @StateObject private var vm = CharacterViewModel()
+    @Environment(\.displayScale) var scale
+    let size: CGFloat = 180
+
+    let columns = [
+        GridItem(.adaptive(minimum: 150), spacing: 30),
+    ]
 
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-        List {
-            ForEach(vm.characters) { character in
-                Text(character.name)
-            }
+        ScrollView {
+            LazyVGrid(columns: columns, spacing: 10) {
+                ForEach(vm.characters) { character in
+                    VStack {
+                        AsyncImage(url: URL(string: character.thumbnail["path"]! + "." + character
+                                       .thumbnail["extension"]!),
+                        scale: scale * size) { phase in
+                            switch phase {
+                            case .empty:
+                                ZStack {
+                                    Color.gray
+                                    ProgressView()
+                                }
+                            case let .success(image):
+                                image.resizable()
+
+                            case let .failure(error):
+                                Text(error.localizedDescription)
+
+                            @unknown default:
+                                EmptyView()
+                            }
+                        }
+                        .frame(width: size, height: size)
+                        HStack {
+                            Text(character.name)
+                                .lineLimit(1)
+                                .truncationMode(.tail)
+
+                            Spacer()
+                        }
+                        .padding([.bottom, .leading], 5)
+                    }
+                    .cornerRadius(20)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 20)
+                            .stroke(.gray, lineWidth: 1)
+                    )
+                }
+            }.padding(20)
         }.task {
             do {
                 try await vm.fetchCharacters()
